a21f98f1261a2086d0cf03d28dcfc5e34a3697fd
조경진 <kj_cho@idealidea.co.kr>
2020-07-07 21:50:18 +0900

    연습용 방 목록 직접 추가

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
@@ -2,8 +2,11 @@ package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
 
 class MainActivity : BaseActivity() {
+
+    val mRoomList = ArrayList<Room>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,16 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mRoomList.add(Room(8000, "서울시 마포구", 5, "첫번째 방 입니다."))
+        mRoomList.add(Room(25000, "서울시 마포구", 0, "두번째 방 입니다."))
+        mRoomList.add(Room(7800, "서울시 마포구", -1, "세번째 방 입니다."))
+        mRoomList.add(Room(53000, "서울시 강서구", 2, "네번째 방 입니다."))
+        mRoomList.add(Room(27000, "서울시 강서구", 3, "다섯번째 방 입니다."))
+        mRoomList.add(Room(15000, "서울시 강서구", 0, "여섯번째 방 입니다."))
+        mRoomList.add(Room(4000, "서울시 종로구", -2, "일곱번째 방 입니다."))
+        mRoomList.add(Room(3000, "서울시 종로구", 7, "여덟번째 방 입니다."))
+        mRoomList.add(Room(44000, "서울시 종로구", 15, "아홉번째 방 입니다."))
 
     }
 
