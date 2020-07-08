9fb1b9b99adabf6eebabec95372f0bc8ade4db55
조경진 <kj_cho@idealidea.co.kr>
2020-07-08 22:12:57 +0900

    스플래쉬 화면에서 3초간 머물다 메인으로 이동

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/SplashActivity.kt
@@ -1,7 +1,9 @@
 package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,23 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        지연 처리를 해주는 핸들러 변수
+        val myHandler = Handler()
+
+//        핸들러 변수를 이용해서, 3초 후에 { } 내부의 내용 실행하도록
+        myHandler.postDelayed({
+
+//          Intent로 화면 이동
+            val myIntent = Intent(mContext, MainActivity::class.java)
+            startActivity(myIntent)
+
+//            넘기고 나서는 로딩화면 종료
+            finish()
+
+        }, 3000)
+
+
 
     }
 
