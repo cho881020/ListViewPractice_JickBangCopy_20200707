de9b81a4d0f15d36c89eaed7dd28d5ea3f2d5e20
조경진 <kj_cho@idealidea.co.kr>
2020-07-08 20:49:19 +0900

    통째로 넘겨준 방 데이터 활용 => 실제 방 정보 출력

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/ViewRoomDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/ViewRoomDetailActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
 
 class ViewRoomDetailActivity : BaseActivity() {
@@ -19,6 +20,11 @@ class ViewRoomDetailActivity : BaseActivity() {
 
     override fun setValues() {
         val receivedRoom = intent.getSerializableExtra("room") as Room
+
+        priceTxt.text = receivedRoom.getFormattedPrice()
+        descTxt.text = receivedRoom.description
+        addressTxt.text = receivedRoom.address
+        floorTxt.text = receivedRoom.getFormattedFloor()
 
     }
 
